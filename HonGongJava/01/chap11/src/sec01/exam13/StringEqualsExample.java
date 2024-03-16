62f3d37fcb839f63205a5bc9ef957aa4d054d9de
RunKaRin <dlrjswnrla@naver.com>
2024-03-16 22:31:33 +0900

    2024.03.16 10:31 / 문자열 비교 - equals()

---
+++ HonGongJava/01/chap11/src/sec01/exam13/StringEqualsExample.java
@@ -0,0 +1,20 @@
+package sec01.exam13;
+
+public class StringEqualsExample {
+    public static void main(String[] args) {
+        String str1 = new String("A");
+        String str2 = "A";
+
+        if (str1 == str2) {
+            System.out.println("같은 String 객체를 참조");
+        } else {
+            System.out.println("다른 String 객체를 참조");
+        }
+
+        if (str1.equals(str2)) {
+            System.out.println("같은 문자열을 가짐");
+        } else {
+            System.out.println("다른 문자열을 가짐");
+        }
+    }
+}
